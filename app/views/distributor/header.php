cf050a5ea84c42b0e9bee4993d46c18c8ab26588
alvin <maulanaalvin1@gmail.com>
2023-07-12 15:11:24 +0800

    fix tampilan

--- app/views/distributor/header.php
+++ app/views/distributor/header.php
@@ -12,6 +12,7 @@
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="<?= BASEURL ?>/assets/css/project.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
     <!-- link CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css" />
--- app/views/retail/footer.php
+++ app/views/retail/footer.php
@@ -7,7 +7,7 @@
 <!-- Bootstrap core JS-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Core theme JS-->
-<script src="js/scripts.js"></script>
+<script src="<?= BASEURL ?>/assets/js/scripts.js"></script>
 <!-- JS Libraies -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <?php Flasher::flash(); ?>
